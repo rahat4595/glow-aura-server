8cba042686ed6cc7502021598a10b2fc2bb90a1f
Mohammad Rahat <mdrahat4595001@gmail.com>
2024-08-18 15:55:51 +0600

    added sort functionality

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://<username>:<password>@cluster0.ev60phs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ev60phs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,12 +25,55 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productsCollection = client.db("glowDb").collection("products");
+
+
+    //  app.get("/allProducts", async (req, res) => {
+    //   const result = await productsCollection.find().toArray();
+    //   res.send(result);
+    // });
+
+    app.get("/allProducts", async (req, res) => {
+        const { brand, category, sortBy, priceMin, priceMax, search } = req.query;
+        const query = {};
+  
+        if (search) {
+          query.productName = { $regex: search, $options: "i" };
+        }
+  
+        if (brand) {
+          query.brand = brand;
+        }
+        if (category) {
+          query.category = category;
+        }
+  
+        if (priceMin || priceMax) {
+          query.price = {};
+          if (priceMin) {
+            query.price.$gte = parseFloat(priceMin);
+          }
+          if (priceMax) {
+            query.price.$lte = parseFloat(priceMax);
+          }
+        }
+  
+       
+  
+        const result = await productsCollection.find(query)
+          .sort(sortOption)
+          .toArray();
+        res.send(result);
+      });
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
